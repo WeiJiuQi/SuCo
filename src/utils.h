17f17e25e73c5cdd60bc44b7909fd220c8b4006d
Jiuqi Wei <30471793+WeiJiuQi@users.noreply.github.com>
2025-08-06 11:53:37 +0800

    Refine code.

--- src/utils.h
+++ src/utils.h
@@ -8,15 +8,43 @@
 
 using namespace std;
 
-struct hash_pair { 
-    template <class T1, class T2> 
-    size_t operator()(const std::pair<T1, T2>& p) const
-    { 
-        auto hash1 = std::hash<T1>{}(p.first); 
-        auto hash2 = std::hash<T2>{}(p.second); 
-        return hash1 ^ hash2; 
-    } 
-}; 
+// struct hash_pair { 
+//     template <class T1, class T2> 
+//     size_t operator()(const std::pair<T1, T2>& p) const
+//     { 
+//         auto hash1 = std::hash<T1>{}(p.first); 
+//         auto hash2 = std::hash<T2>{}(p.second); 
+//         return hash1 ^ hash2; 
+//     } 
+// }; 
+
+template <typename T>
+inline void hash_combine(std::size_t &seed, const T &val) {
+    seed ^= std::hash<T>()(val) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+}
+// auxiliary generic functions to create a hash value using a seed
+template <typename T> inline void hash_val(std::size_t &seed, const T &val) {
+    hash_combine(seed, val);
+}
+template <typename T, typename... Types>
+inline void hash_val(std::size_t &seed, const T &val, const Types &... args) {
+    hash_combine(seed, val);
+    hash_val(seed, args...);
+}
+
+template <typename... Types>
+inline std::size_t hash_val(const Types &... args) {
+    std::size_t seed = 0;
+    hash_val(seed, args...);
+    return seed;
+}
+
+struct hash_pair {
+    template <class T1, class T2>
+    std::size_t operator()(const std::pair<T1, T2> &p) const {
+        return hash_val(p.first, p.second);
+    }
+};
 
 static size_t getCurrentRSS() {
 #if defined(_WIN32)
