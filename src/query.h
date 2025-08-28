4ccc4daefc64a5c03172c180ad38331141d40f58
Jiuqi Wei <30471793+WeiJiuQi@users.noreply.github.com>
2025-08-28 09:35:42 +0800

    Update query.h

--- src/query.h
+++ src/query.h
@@ -13,6 +13,12 @@
 
 using namespace std;
 
+struct Compare {
+    bool operator()(const std::pair<float, int>& a, const std::pair<float, int>& b) {
+        return a.first > b.first;
+    }
+};
+
 void ann_query(float ** &dataset, int ** &queryknn_results, long int dataset_size, int data_dimensionality, int query_size, int k_size, float ** &querypoints, vector<unordered_map<pair<int, int>, vector<int>, hash_pair>> &indexes, float * &centroids_list, int subspace_num, int subspace_dimensionality, int kmeans_num_centroid, int kmeans_dim, int collision_num, int candidate_num, int number_of_threads, long int &query_time);
 
 void dynamic_activate(vector<unordered_map<pair<int, int>, vector<int>, hash_pair>> &indexes, vector<pair<int, int>> &retrieved_cell, vector<float> &first_half_dists, vector<int> &first_half_idx, vector<float> &second_half_dists, vector<int> &second_half_idx, int collision_num, int kmeans_num_centroid, int subspace_idx);
